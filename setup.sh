5733b19d0cad0f40e99f73daebaba4a24ae5ec47
ta-chibana <tachibana.gmc@gmail.com>
2016-11-02 00:57:11 +0900

    Add .vim

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.bash_profile .bashrc .bash_aliases .vimrc)
+DOT_FILES=(.bash_profile .bashrc .bash_aliases .vimrc .vim)
 
 for file in ${DOT_FILES[@]}; do
     ln -s $HOME/dotfiles/$file $HOME/$file && echo "Succeeded to create ${file}"
