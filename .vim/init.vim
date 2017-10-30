51c8561acd22571e7b3a398efa274d6bd31697a8
ta-chibana <tachibana.gmc@gmail.com>
2017-10-30 21:53:10 +0900

    Add neovim settings

---
+++ .vim/init.vim
@@ -0,0 +1,1 @@
+/Users/chibana/dotfiles/.vimrc
--- setup.sh
+++ setup.sh
@@ -9,8 +9,23 @@ done
 
 if [ ! -e $HOME/.vim ]; then
   ln -s $HOME/dotfiles/$DOT_VIM/ $HOME/$DOT_VIM && echo "Succeeded to create ${DOT_VIM}"
-  git submodule init
-  git submodule update
 else
   echo "$HOME/${DOT_VIM} is exists"
 fi
+
+if [ ! -e $HOME/.config/nvim ]; then
+  ln -s $HOME/dotfiles/.vim $HOME/.config/nvim
+  echo "Succeeded to create $HOME/.config/nvim"
+else
+  echo "$HOME/.config/nvim is exists"
+fi
+
+if [ ! -e $HOME/.config/nvim/init.vim ]; then
+  ln -s $HOME/dotfiles/.vimrc $HOME/.config/nvim/init.vim
+  echo "Succeeded to create $HOME/.config/nvim/init.vim"
+else
+  echo "$HOME/.config/nvim/init.vim is exists"
+fi
+
+git submodule init
+git submodule update
